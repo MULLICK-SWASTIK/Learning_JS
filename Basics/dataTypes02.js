c0cea1fe0807179f9e609ed54ee209d6f0b2a2b9
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-03-25 16:25:42 +0530

    Added dataTypes02.js

---
+++ Basics/dataTypes02.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser-----maybe allowd in DOM in browser
+
+console.log(3
+    +
+    3) //code readability compromised
+
+console.log("abc")
+
+let name = "swastik"
+let age = 20
+let isLoggedIn = false
+
+//number -> 2 powered 53
+//bigint
+//string -> ""
+//boolean -> true/false
+//null -> standalone value
+//undefined -> value not assigned
+//symbol -> unique
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null); //object
