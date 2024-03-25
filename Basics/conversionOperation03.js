3e56c62b95e1455222a3fd1de8b3da825433b75c
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-03-26 02:26:01 +0530

    Updated conversionOperation03.js

--- Basics/conversionOperation03.js
+++ Basics/conversionOperation03.js
@@ -1,27 +1,56 @@
-// let score = 33
-// let score = "33"
-// let score = "33abc"
-// let score = null
-// let score = undefined
-// let score = "abc"
-let score = true
+// // let score = 33
+// // let score = "33"
+// // let score = "33abc"
+// // let score = null
+// // let score = undefined
+// // let score = "abc"
+// let score = true
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueNumber = Number(score)
+// let valueNumber = Number(score)
 
-console.log(typeof valueNumber);
-// console.log(valueNumber+" - Not a Number");
-console.log(valueNumber);
+// console.log(typeof valueNumber);
+// // console.log(valueNumber+" - Not a Number");
+// console.log(valueNumber);
 
-/*
-"33" -> 33
-"33abc" -> NaN
-true -> 1; false ->0
-*/
+// /*
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1; false ->0
+// */
 
-// let isLoggedIn = 1
-let isLoggedIn = "swastik"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// // let isLoggedIn = 1
+// let isLoggedIn = "swastik"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// /*
+// 1 -> true; 0 -> false
+// "" -> false; "swastik" -> true
+// */
+
+// let someNum=33
+// let stringNum=String(someNum)
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+//select all the code above at once to "uncomment"
+// -----------------------------OPERATIONS------------------------------------------
+
+let value = 3
+let negVal=-value
+console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="hello"
+let str2=" mullick"
+let str3=str1+str2
+console.log(str3);
