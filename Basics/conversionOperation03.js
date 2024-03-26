7dd159d69fbb8de0eada5ea5982914c9634dbefa
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-03-27 01:00:32 +0530

    Updated conversionOperation03.js

--- Basics/conversionOperation03.js
+++ Basics/conversionOperation03.js
@@ -40,7 +40,7 @@
 // -----------------------------OPERATIONS------------------------------------------
 
 let value = 3
-let negVal=-value
+let negVal = -value
 console.log(negVal);
 
 // console.log(2+2);
@@ -50,7 +50,25 @@ console.log(negVal);
 // console.log(2/2);
 // console.log(2%2);
 
-let str1="hello"
-let str2=" mullick"
-let str3=str1+str2
+let str1 = "hello"
+let str2 = " mullick"
+let str3 = str1 + str2
 console.log(str3);
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log("1" + (2 + 2));
+console.log(1 + 2 + "2");
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 //Bad practice
+
+let gameCounter=100
+// gameCounter++
+++gameCounter
+console.log(gameCounter);
