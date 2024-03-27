5c4efe21c2f4e459bb29c8f0118ed7ca48e98a14
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-03-27 19:56:05 +0530

    Updated datatypes-summary.js

--- Basics/datatypes-summary.js
+++ Basics/datatypes-summary.js
@@ -1,39 +1,61 @@
-// Primitive
+// // Primitive
 
-/* 7 types : 
-String
-Number
-Boolean
-null
-undefined
-Symbol
-BigInt
-*/
-const score = 100
-const scoreValue = 3
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
-const id = Symbol('123')
-const diffId = Symbol('123')
-console.log(id === diffId);
+// /* 7 types : 
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+// */
+// const score = 100
+// const scoreValue = 3
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+// const id = Symbol('123')
+// const diffId = Symbol('123')
+// console.log(id === diffId);
 
-// const bigNumber = 1313213212156466468778317821n
+// // const bigNumber = 1313213212156466468778317821n
 
-// Reference (Non primitive)
-// Array, Objects, Functions
+// // Reference (Non primitive)
+// // Array, Objects, Functions
 
-const heros = ["kld", "jdsf", "sdkf"]
-let myObj = {
-    name: "swastik",
-    age: 20
+// const heros = ["kld", "jdsf", "sdkf"]
+// let myObj = {
+//     name: "swastik",
+//     age: 20
+// }
+
+// const myfunction = function () {
+//     console.log("Hello World");
+// }
+
+// // console.log(typeof bigNumber);
+// // console.log(typeof outsideTemp);
+// // console.log(typeof myfunction);
+// console.log(typeof heros);
+
+//----------------------------------------------------------------------------------------------------------------
+// Stack (Primitive) : gets a copy, Heap (Non-Primitive) : gets a reference
+
+
+let myGitHubName = "mullickdashswastik"
+let anotherName = myGitHubName
+anotherName = "mullick-swastik"
+
+console.log(myGitHubName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
 }
 
-const myfunction = function () {
-    console.log("Hello World");
-}
+let userTwo = userOne
+userTwo.email = "swastik@google.com"
 
-// console.log(typeof bigNumber);
-// console.log(typeof outsideTemp);
-// console.log(typeof myfunction);
-console.log(typeof heros);
+console.log(userOne.email);
+console.log(userTwo.email);
