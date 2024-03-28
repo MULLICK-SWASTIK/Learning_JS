0fc080c2c005073cc0e3c06dcd26cc2edcbf8afa
MULLICK-SWASTIK <swastik08122004mullick@gmail.com>
2024-03-28 14:57:00 +0530

    Added Basics02 folder and array01.js file in it

---
+++ Basics02/arrays01.js
@@ -0,0 +1,48 @@
+// // array
+
+const myarr = [0, 1, 2, 3, 4, 5, true, "swastik"]
+// const myHeros = ["sdkf", "asdkfh", "skdjfh"]
+// const newAr = Array(1, 2, 3, 4)
+
+// console.log(myarr[2]);
+
+// // Array methods
+
+// // myarr.push(6)
+// // myarr.push(7)
+// // console.log(myarr);
+// // myarr.pop()
+// // myarr.unshift(9)
+// // myarr.shift()
+// // console.log(myarr.includes(9))
+// // console.log(myarr.indexOf(9));
+// // console.log(myarr.indexOf(4));
+
+// const a1=myarr.join()
+
+// console.log(myarr);
+// console.log(a1);
+// console.log(typeof a1);
+
+//slice, splice
+
+console.log("a ", myarr);
+const mya=myarr.slice(1,3)
+console.log(mya);
+console.log("b ",myarr);
+
+const myb=myarr.splice(1,3)
+console.log(myb);
+console.log("c ",myarr);
+
+
+
+
+
+
+
+
+
+
+
+
